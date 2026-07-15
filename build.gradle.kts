13dff2920eb2c85e02dc51ebb2dfb36bc03d916c
jpz <juhpaza@gmail.com>
2026-07-15 19:26:49 +0300

    Add Gradle wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,6 @@ plugins {
 group = "fi.juhpaza"
 version = "0.1.0-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
-
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
 
